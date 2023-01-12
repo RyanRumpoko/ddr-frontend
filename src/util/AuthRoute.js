829d177720d8e3b831393a67f28a38d32416e3af
unknown <xiipa4ryanrumpoko@gmail.com>
2023-01-12 21:38:19 +0700

    Add customer file

--- src/util/AuthRoute.js
+++ src/util/AuthRoute.js
@@ -1,19 +1,22 @@
-import React from 'react'
+import React, { useContext } from 'react'
 import { Navigate } from 'react-router-dom'
+import { AuthContext } from 'src/context/auth'
 
 const AuthRoute = ({ children }) => {
+  const { user } = useContext(AuthContext)
   const checkToken = localStorage.getItem('jwtToken')
 
-  if (!checkToken) {
+  if (!checkToken && !user) {
     return <Navigate to="/login" />
   }
   return children
 }
 
 const AuthLogin = ({ children }) => {
+  const { user } = useContext(AuthContext)
   const checkToken = localStorage.getItem('jwtToken')
 
-  if (checkToken) {
+  if (checkToken && user) {
     return <Navigate to="/" />
   }
   return children
